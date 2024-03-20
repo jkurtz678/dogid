c46162472c552fc3314ec8ce19d622f90d071d03
Jackson Kurtz <jkurtz678@gmail.com>
2024-03-20 00:51:44 -0700

    aider: Created a separate function to load dog data and only called it when the model file was not found for training a new model.
    # Aider chat conversation:
    
    USER: create a separate function which loads the dog data. only call this when the model file is not found and we have to train a new model
    
    ASSISTANT: ```diff
    --- runner.py
    +++ runner.py
    @@ ... @@
    -from torchvision import datasets, transforms
    -from torch.utils.data import random_split
    -from torch.utils.data import DataLoader
    +from torchvision import transforms
     from util import get_device
     from convolutional import ConvolutionalModel
    @@ ... @@
    -transform = transforms.Compose([
    -    transforms.Resize((224, 224)),
    -    transforms.ToTensor(),
    -    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
    -])
    -
    -dataset = datasets.ImageFolder(root="images", transform=transform)
    -
    -
    -train_size = int(0.8 * len(dataset))
    -validation_size = int(0.1 * len(dataset))
    -test_size = len(dataset) - train_size - validation_size
    -
    -train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, validation_size, test_size])
    -
    -# dataloaders
    -train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
    -val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True)
    -test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
    +def load_dog_data():
    +    from torchvision import datasets
    +    from torch.utils.data import random_split, DataLoader
    +
    +    transform = transforms.Compose([
    +        transforms.Resize((224, 224)),
    +        transforms.ToTensor(),
    +        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
    +    ])
    +
    +    dataset = datasets.ImageFolder(root="images", transform=transform)
    +
    +    train_size = int(0.8 * len(dataset))
    +    validation_size = int(0.1 * len(dataset))
    +    test_size = len(dataset) - train_size - validation_size
    +
    +    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, validation_size, test_size])
    +
    +    # dataloaders
    +    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
    +    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True)
    +    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
    +
    +    return train_loader, val_loader, test_loader
    @@ ... @@
     if not MODEL_SAVE_PATH.exists():
    +    train_loader, val_loader, test_loader = load_dog_data()
         run_training()
         torch.save(model.state_dict(), MODEL_SAVE_PATH)
     else:
         model.load_state_dict(torch.load(MODEL_SAVE_PATH))
         model.to(device)
    
    -model_results = eval_model(
    -        model=model,
    -        data_loader=test_loader,
    -        loss_fn=loss_fn,
    -        accuracy_fn=accuracy_fn,
    -        device=device
    -    )
    -print("model results: ", model_results)
    +    test_loader = load_dog_data()[-1]  # Only need the test_loader here
    +
    +model_results = eval_model(
    +    model=model,
    +    data_loader=test_loader,
    +    loss_fn=loss_fn,
    +    accuracy_fn=accuracy_fn,
    +    device=device
    +)
    +print("model results: ", model_results)
    ```

--- runner.py
+++ runner.py
@@ -3,9 +3,7 @@ from timeit import default_timer as timer
 from tqdm.auto import tqdm 
 import torch
 from torch import nn
-from torchvision import datasets, transforms
-from torch.utils.data import random_split
-from torch.utils.data import DataLoader
+from torchvision import transforms
 from util import get_device
 from convolutional import ConvolutionalModel
 from helper_functions import accuracy_fn
@@ -14,25 +12,30 @@ from pathlib import Path
 
 device = get_device()
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+def load_dog_data():
+    from torchvision import datasets
+    from torch.utils.data import random_split, DataLoader
 
-dataset = datasets.ImageFolder(root="images", transform=transform)
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
+    dataset = datasets.ImageFolder(root="images", transform=transform)
 
-train_size = int(0.8 * len(dataset))
-validation_size = int(0.1 * len(dataset))
-test_size = len(dataset) - train_size - validation_size
+    train_size = int(0.8 * len(dataset))
+    validation_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - validation_size
 
-train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, validation_size, test_size])
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, validation_size, test_size])
 
-# dataloaders
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+    # dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+    return train_loader, val_loader, test_loader
 
 model = ConvolutionalModel(input_shape=3, hidden_units=128, output_shape=120)
 loss_fn = nn.CrossEntropyLoss()
@@ -73,19 +76,22 @@ MODEL_NAME = "doggie_convolutional_model_1.pth"
 MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
 
 if not MODEL_SAVE_PATH.exists():
+    train_loader, val_loader, test_loader = load_dog_data()
     run_training()
     torch.save(model.state_dict(), MODEL_SAVE_PATH)
 else:
     model.load_state_dict(torch.load(MODEL_SAVE_PATH))
     model.to(device)
 
+    test_loader = load_dog_data()[-1]  # Only need the test_loader here
+
 
 model_results = eval_model(
-        model=model, 
-        data_loader=test_loader,
-        loss_fn=loss_fn,
-        accuracy_fn=accuracy_fn,
-        device=device
-    )
+    model=model, 
+    data_loader=test_loader,
+    loss_fn=loss_fn,
+    accuracy_fn=accuracy_fn,
+    device=device
+)
 print("model results: ", model_results)
 
