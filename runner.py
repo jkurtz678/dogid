3e736a9d2171b276493cad547e507a38612cb4b0
Jackson Kurtz <jkurtz678@gmail.com>
2024-03-31 13:49:10 -0700

    refactor main

--- runner.py
+++ runner.py
@@ -10,7 +10,6 @@ from helper_functions import accuracy_fn
 from training import train_step, test_step, eval_model
 from pathlib import Path
 
-device = get_device()
 
 def load_dog_data():
     from torchvision import datasets
@@ -37,60 +36,74 @@ def load_dog_data():
 
     return train_loader, val_loader, test_loader
 
-model = ConvolutionalModel(input_shape=3, hidden_units=128, output_shape=120)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
-
-def run_training():
-    train_time_start = timer()
-    epochs = 5
-    for epoch in tqdm(range(epochs)):
-        print(f"Epoch: {epoch}\n-------------")
-
-        train_loss, train_acc = train_step(data_loader=train_loader,
-                   model=model,
-                   loss_fn=loss_fn,
-                   optimizer=optimizer,
-                   accuracy_fn=accuracy_fn,
-                   device=device
-        )
-        
-        test_loss, test_acc = test_step(data_loader=test_loader,
-                  model=model,
-                  loss_fn=loss_fn,
-                  accuracy_fn=accuracy_fn,
-                  device=device,
-        )
-        print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}")
-        print(f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.2f}")
 
 
-    train_time_end = timer()
-    total_time = train_time_end - train_time_start 
-    print(f"Train time on {device}: {total_time:.3f} seconds")
+def run():
 
-MODEL_PATH = Path("models")
-MODEL_PATH.mkdir(exist_ok=True)
-MODEL_NAME = "doggie_convolutional_model_1.pth"
-MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
-
-if not MODEL_SAVE_PATH.exists():
-    train_loader, val_loader, test_loader = load_dog_data()
-    run_training()
-    torch.save(model.state_dict(), MODEL_SAVE_PATH)
-else:
-    model.load_state_dict(torch.load(MODEL_SAVE_PATH))
-    model.to(device)
-
-    test_loader = load_dog_data()[-1]  # Only need the test_loader here
+    device = get_device()
+    model = ConvolutionalModel(input_shape=3, hidden_units=128, output_shape=120)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
 
 
-model_results = eval_model(
-    model=model, 
-    data_loader=test_loader,
-    loss_fn=loss_fn,
-    accuracy_fn=accuracy_fn,
-    device=device
-)
-print("model results: ", model_results)
+    def run_training():
+        train_time_start = timer()
+        epochs = 5
+        for epoch in tqdm(range(epochs)):
+            print(f"Epoch: {epoch}\n-------------")
+
+            train_loss, train_acc = train_step(data_loader=train_loader,
+                       model=model,
+                       loss_fn=loss_fn,
+                       optimizer=optimizer,
+                       accuracy_fn=accuracy_fn,
+                       device=device
+            )
+            
+            test_loss, test_acc = test_step(data_loader=test_loader,
+                      model=model,
+                      loss_fn=loss_fn,
+                      accuracy_fn=accuracy_fn,
+                      device=device,
+            )
+            print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}")
+            print(f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.2f}")
+
+
+        train_time_end = timer()
+        total_time = train_time_end - train_time_start 
+        print(f"Train time on {device}: {total_time:.3f} seconds")
+
+    MODEL_PATH = Path("models")
+    MODEL_PATH.mkdir(exist_ok=True)
+    MODEL_NAME = "doggie_convolutional_model_1.pth"
+    MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+    if not MODEL_SAVE_PATH.exists():
+        print("No saved model found, starting training...")
+        train_loader, val_loader, test_loader = load_dog_data()
+        run_training()
+        torch.save(model.state_dict(), MODEL_SAVE_PATH)
+    else:
+        print("Saved model found, skipping training, loading model...")
+        model.load_state_dict(torch.load(MODEL_SAVE_PATH))
+        model.to(device)
+
+        test_loader = load_dog_data()[-1]  # Only need the test_loader here
+
+
+    print("Evaluating model...")
+    model_results = eval_model(
+        model=model, 
+        data_loader=test_loader,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn,
+        device=device
+    )
+    print("model results: ", model_results)
+
+
+# if this is the main file, run the run function
+if __name__ == "__main__":
+    run()
 
